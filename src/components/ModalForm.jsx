c1b2317b0edf4b99e919fbf828c755ef831ee6be
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-09-25 00:20:23 -0500

    feat: Add style grid for inputs form

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -1,17 +1,28 @@
-"use client";
+
 
 import { Button, Modal } from "flowbite-react";
 import { useState } from "react";
+import { useTranslation } from 'react-i18next';
 import FormField from "./FormField";
 
 export function ModalFormulario() {
   const [openModal, setOpenModal] = useState(false);
+  const [foto, setFoto] = useState(null);
+  const { t } = useTranslation();
+
+  const handleFotoChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFoto(URL.createObjectURL(file)); // Crear una URL temporal para la imagen
+    }
+  };
+
   return (
     <>
       <div className="snap-center">
         <div className="w-40">
         </div>
-        <Button className= "bg-[#fca5a5] text-black" onClick={() => setOpenModal(true)}>Realizar Publicación</Button>
+        <Button className= "bg-[#fca5a5] text-black" onClick={() => setOpenModal(true)}>{t("functionButton")}</Button>
       </div>
 
       <Modal
@@ -19,66 +30,110 @@ export function ModalFormulario() {
 
         onClose={() => setOpenModal(false)}
       >
-        <Modal.Header className="bg-[#F5E1DC]"><p className="text-[#DA6274] items-center">¿A quien debo buscar?</p></Modal.Header>
+        <Modal.Header className="bg-[#F5E1DC]"><p className="text-[#DA6274] items-center">{t("titleFormPublish")}</p></Modal.Header>
         <Modal.Body className="bg-[#F5E1DC]">
           <div className="space-y-4 p-2">
+
+            <div className="grid grid-cols-2 gap-4">
             <FormField
-              label="Nombre"
-              id="nombre"
+              label={t("nameLabel")}
+              id="name"
               type="text"
-              placeholder="Escribe el nombre del animal"
+              placeholder={t("namePlaceholder")}
             />
             <FormField
-              label="Tipo de animal"
+              label={t("animalTypeLabel")}
               id="tipo-animal"
               type="text"
-              placeholder="Ej: Perro, Gato..."
+              placeholder={t("animalTypeplaceholder")}
             />
+            </div>
+
             <FormField
-              label="Género"
+              label={t("genderLabel")}
               id="genero"
               type="radio"
               options={[
-                { label: "Macho", value: "macho" },
-                { label: "Hembra", value: "hembra" },
+                { label: t("genderOptionsLabel1") , value: "macho" },
+                { label: t("genderOptionsLabel2"), value: "hembra" },
               ]}
             />
             <FormField
-              label="Características"
+              label={t("characteristicsLabel")}
               id="caracteristicas"
               type="textarea"
-              placeholder="Describe las características del animal"
+              placeholder={t("characteristicsPlaceholder")}
             />
+
+            <div className="grid grid-cols-2 gap-4">
+
             <FormField
-              label="Tamaño"
+              label={t("sizeLabel")}
               id="tamano"
               type="text"
-              placeholder="Ej: Pequeño, Mediano, Grande"
+              placeholder={t("sizePlaceholder")}
             />
-            <FormField
-              label="Edad"
+             <FormField
+              label={t("ageLabel")}
               id="edad"
               type="number"
-              placeholder="Edad del animal"
+              placeholder={t("agePlaceholder")}
             />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+
             <FormField
-              label="Recompensa"
-              id="recompensa"
-              type="number"
-              placeholder="Si hay recompensa, ingrésala"
-            />
+                label={t("dayAndTimeLabel")}
+                id="dia-hora"
+                type="text"  // O usa 'datetime-local' para entrada de fecha y hora
+                placeholder={t("dayAndTimePlaceholder")}
+              />
+              <FormField
+                label={t("rewardLabel")}
+                id="recompensa"
+                type="number"
+                placeholder={t("rewardPlaceholder")}
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+                <FormField
+                  label={t("contactLabel")}
+                  id="contacto"
+                  type="text"
+                  placeholder={t("contactPlaceholder")}
+                />
+                <div className="flex justify-center ">
+                  <Button className="bg-[#A7C7E7] text-black flex items-center justify-center">{t("markLocationButton")}</Button>
+                </div>
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700">{t("photoLabel")}</label>
+              <div className="flex items-center justify-center">
+                {foto ? (
+                  <img src={foto} alt="Foto" className="w-32 h-32 object-cover border" />
+                ) : (
+                  <div className="w-32 h-32 flex items-center justify-center bg-gray-200 border">
+
+                  </div>
+                )}
+              </div>
+              <input type="file" onChange={handleFotoChange} className="mt-2" />
+            </div>
+
             <FormField
-              label="Contacto"
-              id="contacto"
+              label={t("tagsLabel")}
+              id="etiquetas"
               type="text"
-              placeholder="Número de teléfono o email"
+              placeholder={t("tagsPlaceholder")}
             />
           </div>
         </Modal.Body>
         <Modal.Footer className="flex justify-center gap-8 bg-[#F5E1DC]">
-          <Button onClick={() => setOpenModal(false)} className="bg-[#14AE5C] text-black">Publicar</Button>
-          <Button color="gray" onClick={() => setOpenModal(false)} className="bg-[#FF859B] text-black">
-            Cancelar
+          <Button onClick={() => setOpenModal(false)} className="bg-[#14AE5C] text-black">{t("BotonPublicar")}</Button>
+          <Button onClick={() => setOpenModal(false)} className="bg-[#FF859B] text-black">
+            {t("BotonDenegar")}
           </Button>
         </Modal.Footer>
       </Modal>
--- src/views/Feed.jsx
+++ src/views/Feed.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Modal from '../components/ModalForm';
-import Form from '../components/Form';
 
 const Feed = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -11,7 +10,6 @@ const Feed = () => {
     <div className="App">
 
       <Modal show={isModalOpen} onClose={toggleModal}>
-        <Form />
       </Modal>
     </div>
   );
