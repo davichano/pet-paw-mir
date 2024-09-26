3dc3343e5ea9cb5056d8b751f3ebab17f89b7a06
William Lupaca <wlupacat@gmail.com>
2024-09-26 09:43:27 -0500

    feat: hide Header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import {PetContext} from "./contexts/PetContext"
 import { MainRouter } from './routers/MainRouter'
 import { I18nextProvider } from "react-i18next";
 import i18n from './i18n.jsx'
-import Header from './components/template/Header.jsx'
+
 import { useCurrentUser } from './hooks/useCurrentUser.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const current_user=useCurrentUser();
@@ -15,10 +16,11 @@ function App() {
         <I18nextProvider i18n={i18n}>
           <PetContext.Provider value={{current_user}}>
             <div>
-                <Header/>
-                <MainRouter></MainRouter>
+                <BrowserRouter>
+                  <MainRouter/>
+                </BrowserRouter>
             </div>
-          </PetContext.Provider> 
+          </PetContext.Provider>
         </I18nextProvider>
        </div>
     </>
--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -1,15 +1,19 @@
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Routes, Route, useLocation} from 'react-router-dom';
 import Home from '../views/Home';
 import RegisterForm from '../components/RegisterForm';
+import Header from '../components/template/Header.jsx'
 
 export const MainRouter=()=>{
+  const location = useLocation();
+  const hideHeaderRoutes = ["/register"];
   return(
-    <BrowserRouter>
+    <>
+      {!hideHeaderRoutes.includes(location.pathname) && <Header/>}
       <Routes>
-                <Route path="/" element={<Home></Home>}/>
-                <Route path="/register" element={<RegisterForm/>}/>
-            </Routes>
-    </BrowserRouter>
+        <Route path="/" element={<Home></Home>}/>
+        <Route path="/register" element={<RegisterForm/>}/>
+      </Routes>
+    </>
   )
 }
