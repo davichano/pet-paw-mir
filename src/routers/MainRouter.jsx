ac4757d4a35e84112f4ce68506f1b5c74825ac73
Gabriel Machicao Quispe <139248647+GabrielMaQui@users.noreply.github.com>
2024-09-26 22:39:31 -0500

    feat: Add desing page Details Publication (#4)
    Co-authored-by: Gabriel Steven Machicao Quispe <legard2911@gmail.com>

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -2,6 +2,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from '../views/Home';
 import Feed from '../views/Feed';
+import DetailsPublication from '../views/DetailsPublication';
 
 export const MainRouter=()=>{
   return(
@@ -9,7 +10,8 @@ export const MainRouter=()=>{
       <Routes>
                 <Route path="/" element={<Home></Home>}/>,
                 <Route path="/feed" element={<Feed></Feed>}/>
+                <Route path="/description" element={<DetailsPublication></DetailsPublication>}/>
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
   )
 }
