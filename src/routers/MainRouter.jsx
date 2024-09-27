9eecb3becaab2744f8c1c8c77b8549304f1b5c93
davichano <dparedesa@unc.edu.pe>
2024-09-26 23:09:30 -0500

    Mirpg 4 editar perfil (#5)
    * index on MIRPG-4-editar-perfil: dcb8c40 Mirpg 5 publicar mascota (#2)
    
    * feat: user profile + fake server
    
    ---------
    
    Co-authored-by: David Paredes <d.paredes@didepti.com>

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from '../views/Home';
 import Feed from '../views/Feed';
 import DetailsPublication from '../views/DetailsPublication';
+import UserProfile from '../views/users/Profile';
 
 export const MainRouter=()=>{
   return(
@@ -11,6 +12,7 @@ export const MainRouter=()=>{
                 <Route path="/" element={<Home></Home>}/>,
                 <Route path="/feed" element={<Feed></Feed>}/>
                 <Route path="/description" element={<DetailsPublication></DetailsPublication>}/>
+                <Route path="/user/:id" element={<UserProfile></UserProfile>}/>
       </Routes>
       </BrowserRouter>
   )
