92146370535d886e30674b9dcf7cc5597f4cb535
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-10-02 18:24:05 -0500

    refactor: Add component CartPostPet

--- src/components/DetailsPet/PetInfo.jsx
+++ src/components/DetailsPet/PetInfo.jsx
@@ -27,24 +27,24 @@ const PetInfo = ({ name, size, age, location, dateLost, gender, imageUrl }) => {
 
       </Modal.Header>
 
-      <Modal.Body className="bg-[#ff797d]">
-    <div className="p-4">
-      <h2 className="text-xl font-bold mb-4">{name}</h2>
+      <Modal.Body className="bg-[#ff797d] text-white">
+    <div className="p-1">
+      <h2 className="text-xl font-bold mb-2">{name}</h2>
 
       <div className="flex items-center mb-2">
-        <img src="/src/assets/img/Icons/calendar.svg" alt="calendario" className="w-5 h-5 mr-2"></img>
-        <p>{t("nameLabel")}: {age} {t("longTime")}</p>
+        <img src="/src/assets/img/Icons/calendar.svg" alt="calendario" className="w-10 h-10 mr-2"></img>
+        <p>{t("agePet")}: {age} {t("longTime")}</p>
       </div>
 
 
       <div className="flex items-center mb-2">
-        <img src="/src/assets/img/Icons/rule.svg" alt="tamano" className="w-5 h-5 mr-2"></img>
+        <img src="/src/assets/img/Icons/rule.svg" alt="tamano" className="w-10 h-10 mr-2"></img>
         <p>{t("sizePet")}: {size}</p>
       </div>
 
 
       <div className="flex items-center mb-2">
-      <img src="/src/assets/img/Icons/location.svg" alt="location" className="w-5 h-5 mr-2"></img>
+      <img src="/src/assets/img/Icons/location.svg" alt="location" className="w-10 h-10 mr-2"></img>
         <p>{t("locatePet")}: {location}</p>
       </div>
       <div className="w-full h-auto flex items-center justify-center">
