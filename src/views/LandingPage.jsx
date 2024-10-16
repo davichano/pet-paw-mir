ded3b57e2c085b16bc002534ed09f1b570353c79
William Lupaca Ticona <wlupacat@gmail.com>
2024-10-16 01:37:20 -0500

    fix: fix desktop design for landing page

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,16 +1,30 @@
 import Button from "../components/ui/Button";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import Header from "../components/ui/Header";
 
 const LandingPage = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   return (
     <div>
-      <div className="h-screen bg-custom-50 flex items-center justify-center">
-        <div className="w-full h-full max-w-xl p-6 flex flex-col items-center justify-between">
-          <div className="flex text-custom-200 font-semibold text-3xl">
+      <div>
+        <Header />
+      </div>
+
+      <div className="h-screen md:h-full bg-custom-50 flex items-center justify-center">
+        <div className="hidden w-full h-full max-w-md p-6 md:flex flex-col items-center justify-center m-12 md:max-w-md">
+          <div className="w-full max-h-lg aspect-square h-auto bg-custom-200 items-center justify-center">
+            <div className="text-custom-50 font-bold text-2xl text-center">
+              Aquí ira una imagen
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-full max-w-lg p-6 flex flex-col items-center justify-between md:max-w-md md:justify-center md:gap-28">
+          <div className="flex text-custom-200 font-semibold text-3xl md:hidden">
             <p>Pet Paws</p>
           </div>
-          <div className="max-w-44 max-h-44 aspect-square h-auto bto bg-custom-200 items-center justify-center">
+          <div className="max-w-44 max-h-44 aspect-square h-auto bto bg-custom-200 items-center justify-center md:hidden">
             <div className="text-custom-50 font-bold text-2xl text-center">
               Aquí ira una imagen
             </div>
@@ -22,8 +36,21 @@ const LandingPage = () => {
             {t("landing.subtitle")}
           </p>
           <div className="w-full">
-            <Button>{t("iHaveAccount")}</Button>
-            <Button className="btn-secondary">{t("createAccount")}</Button>
+            <Button
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              {t("iHaveAccount")}
+            </Button>
+            <Button
+              onClick={() => {
+                navigate("/signup");
+              }}
+              className="btn-secondary"
+            >
+              {t("createAccount")}
+            </Button>
           </div>
         </div>
       </div>
