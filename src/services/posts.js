77558325db184d05dc927c5ea38e30522d1a37cf
William Lupaca <wlupacat@gmail.com>
2024-10-18 09:57:48 -0500

    feat: connect publish post with backend

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080/";
+const BASE_URL = "http://localhost:3000/";
+import {formatPostData} from '../helpers/formatPostData';
 
 export async function fetchPosts() {
   const response = await fetch(`${BASE_URL}posts`);
@@ -17,15 +18,22 @@ export async function fetchPostsByUser(id) {
 
 
 export async function createPost(postData) {
-  const response = await fetch(`${BASE_URL}posts`, {
+
+  console.log('postDataFormatted:', formatPostData(postData));
+
+  const postDataFormatted = formatPostData(postData);
+
+  const response = await fetch(`${BASE_URL}api/posts`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
     },
-    body: JSON.stringify(postData),
+    body: JSON.stringify(postDataFormatted),
   });
 
   if (!response.ok) {
+    console.log('Error al crear el post');
     throw new Error('Error al crear el post');
   }
 
