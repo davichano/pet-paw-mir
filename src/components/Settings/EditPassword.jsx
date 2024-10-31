a9a83f0fbff0e2af39fdb6e9fc5feb0d4c76923d
William Lupaca Ticona <wlupacat@gmail.com>
2024-10-31 00:40:45 -0500

    feat: add multilanguage to changePassword component

--- src/components/Settings/EditPassword.jsx
+++ src/components/Settings/EditPassword.jsx
@@ -7,31 +7,33 @@ import FormField from "./ui/FormField";
 import Button from "./ui/Button";
 import Title from "./ui/Title";
 import Description from "./ui/Description";
+import { useTranslation } from "react-i18next";
+import { toast } from "sonner";
 
 // Esquema de validación con Yup
-const schema = yup.object().shape({
-  contrasenaActual: yup.string().required("La contraseña actual es obligatoria"),
-  nuevaContrasena: yup
-    .string()
-    .required('passwordRequired')
-          .min(8, 'passwordLengthError')
-          .matches(/[a-zA-Z]/, 'passwordLetterError')
-          .matches(/\d/, 'passwordNumberError')
-          .matches(/[!@#$%^&*(),.?":{}|<>]/, 'passwordSymbolError'),
-  repetirContrasena: yup
-    .string()
-    .oneOf(
-      [yup.ref("nuevaContrasena"), null],
-      "Las contraseñas deben coincidir"
-    )
-    .required("Debe repetir la nueva contraseña"),
-});
 
 const EditPassword = () => {
+  const { t } = useTranslation();
+  const schema = yup.object().shape({
+    contrasenaActual: yup.string().required(t("currentPasswordRequired")),
+    nuevaContrasena: yup
+      .string()
+      .required(t("newPasswordRequired"))
+      .min(8, t("passwordLengthError"))
+      .matches(/[a-zA-Z]/, t("passwordLetterError"))
+      .matches(/\d/, t("passwordNumberError"))
+      .matches(/[!@#$%^&*(),.?":{}|<>]/, t("passwordSymbolError")),
+    repetirContrasena: yup
+      .string()
+      .oneOf([yup.ref("nuevaContrasena"), null], t("newPasswordsHaveToMatch"))
+      .required(t("repeatNewPassword")),
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm({
     resolver: yupResolver(schema),
   });
@@ -40,7 +42,8 @@ const EditPassword = () => {
 
   const onSubmit = (data) => {
     console.log(data);
-    setIsModalOpen(true);
+    toast.success(t("updatedCorrectly"));
+    reset();
   };
 
   return (
@@ -48,12 +51,30 @@ const EditPassword = () => {
       onSubmit={handleSubmit(onSubmit)}
       className="space-y-6 min-w-64 w-2/4 md:w-96 mx-auto mt-10 bg-white shadow rounded flex flex-col"
     >
-      <Title text="Cambiar Contraseña" />
-      <Description text="Ingresa tu contraseña actual y la nueva contraseña que deseas utilizar." />
-      <FormField label="Contraseña Actual" type="password" id="contrasenaActual" register={register} errors={errors} />
-      <FormField label="Nueva Contraseña" type="password" id="nuevaContrasena" register={register} errors={errors} />
-      <FormField label="Repetir Contraseña" type="password" id="repetirContrasena" register={register} errors={errors} />
-      <Button>Cambiar contraseña</Button>
+      <Title text={t("settings.user.password")} />
+      <Description text={t("settings.user.password.description")} />
+      <FormField
+        label={t("currentPassword")}
+        type="password"
+        id="contrasenaActual"
+        register={register}
+        errors={errors}
+      />
+      <FormField
+        label={t("newPassword")}
+        type="password"
+        id="nuevaContrasena"
+        register={register}
+        errors={errors}
+      />
+      <FormField
+        label={t("repeatNewPassword")}
+        type="password"
+        id="repetirContrasena"
+        register={register}
+        errors={errors}
+      />
+      <Button>{t("settings.user.password")}</Button>
 
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <h2 className="text-xl font-bold">Contraseña Actualizada</h2>
