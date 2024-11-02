2db33e574ae83aef550bd8578cf0e1b2dd673a98
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-11-01 21:44:20 -0500

    fix: correct app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,6 @@ import { I18nextProvider } from "react-i18next";
 import { ConfigProvider } from "./contexts/config/ConfigContext.jsx";
 import { UserProvider } from "./contexts/UserContext.jsx";
 import i18n from "./i18n.jsx";
-
-import { useCurrentUser } from "./hooks/useCurrentUser.jsx";
-import { BrowserRouter } from "react-router-dom";
-import i18n from './i18n.jsx';
 import { useCurrentUser } from './hooks/useCurrentUser.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -21,13 +17,17 @@ function App() {
   return (
     <div className="App bg-white min-h-screen pb-[70px]">
       <I18nextProvider i18n={i18n}>
-        <PetContext.Provider value={{ current_user}}>
-        <SavedPostsProvider>
-          <BrowserRouter>
-            <MainRouter />
-          </BrowserRouter>
-          </SavedPostsProvider>
-        </PetContext.Provider>
+        <UserProvider>
+          <ConfigProvider>
+            <PetContext.Provider value={{ current_user}}>
+              <SavedPostsProvider>
+                <BrowserRouter>
+                  <MainRouter />
+                </BrowserRouter>
+              </SavedPostsProvider>
+            </PetContext.Provider>
+          </ConfigProvider>
+        </UserProvider>
       </I18nextProvider>
     </div>
   );
