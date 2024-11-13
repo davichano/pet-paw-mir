2aff2edbc692420d1e9ac7c06ea1c4bfcf7365c5
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-11-13 01:35:23 -0500

    feat: implements chats for backend

--- src/components/Chat/ChatMessage.jsx
+++ src/components/Chat/ChatMessage.jsx
@@ -1,18 +1,40 @@
 import PropTypes from "prop-types";
 import AvatarChat  from "./AvatarChat";
+import { useEffect, useState } from "react";
+import { fetchUser } from "../../services/users";
 
-const ChatMessage = ({ name, time }) => (
-  <div className="flex items-center gap-3 p-3 hover:bg-pink-50">
-    <AvatarChat />
-    <div className="flex flex-col">
-      <span className="text-custom-350">{name}</span>
-      <span className="text-sm text-custom-200">{time}</span>
+const ChatMessage = ({ userId, time }) => {
+  const [userName, setUserName] = useState("Usuario desconocido");
+
+  useEffect(() => {
+    
+    const getUserData = async () => {
+      try {
+        const user = await fetchUser(userId);
+        setUserName(user.name || "Usuario desconocido");
+      } catch (error) {
+        console.error("Error al obtener el usuario:", error);
+      }
+    };
+
+    if (userId) {
+      getUserData();
+    }
+  }, [userId]);
+
+  return (
+    <div className="flex items-center gap-3 p-3 hover:bg-pink-50">
+      <AvatarChat />
+      <div className="flex flex-col">
+        <span className="text-custom-350">{userName}</span>
+        <span className="text-sm text-custom-200">{time}</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ChatMessage.propTypes = {
-  name: PropTypes.string,
+  userId: PropTypes.string,
   time: PropTypes.string
 }
 
--- src/services/socket.js
+++ src/services/socket.js
@@ -2,13 +2,9 @@ import io from "socket.io-client";
 
 const socket = io("http://localhost:3000/", {transports: ['websocket'],});
 
-
 socket.on("connect", () => {
   console.log("Connected to server");
 });
 
-socket.on("connect_error", (err) => {
-  console.log("Connection failed", err);
-});
 
 export default socket;
