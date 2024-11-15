e6f38be892bed8c08f7529eaf77290e527a8a188
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-11-15 17:50:15 -0500

    feat: create a alternativeHeader for location

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -22,6 +22,7 @@ import ProtectedRoute from "./ProtectedRoute";
 import AdminPage from "../views/AdminPage.jsx";
 import AccessDenied from "../views/AccessDenied.jsx";
 import ChatInterface from "../views/chat/ChatInterface.jsx";
+import AlternateHeader from "../components/Settings/Header.jsx";
 
 
 export const MainRouter = () => {
@@ -36,12 +37,21 @@ export const MainRouter = () => {
     "/settings",
     "/access-denied",
   ];
+
+  const alternateHeaderRoutes = ["/chats", "/post/map", "/settings"]
+
+
   return (
     <>
       <PetProvider>
         <Toaster richColors expand={true} />
-        {!hideHeaderRoutes.includes(location.pathname) && <Header />}
-
+        {!hideHeaderRoutes.includes(location.pathname) &&
+          (alternateHeaderRoutes.includes(location.pathname) ? (
+            <AlternateHeader title={location.pathname} />
+          ) : (
+            <Header />
+          ))
+        }
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
