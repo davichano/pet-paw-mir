da5a1172e557a69d0fcabeb18bcd9ce4f6d233b8
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-11-18 19:38:49 -0500

    fix: correct name for base_server

--- src/services/chat.js
+++ src/services/chat.js
@@ -1,4 +1,6 @@
-const BASE_URL = `${import.meta.env.VITE_BASE_URL}/`;
+const BASE_SERVER = import.meta.env.VITE_BASE_URL;
+const BASE_URL = `${BASE_SERVER}/`;
+
 const token = localStorage.getItem('token');
 
 // Headers personalizados para la autenticación
@@ -6,7 +8,6 @@ const HEADERS = {
   "Content-Type": "application/json",
   "Authorization": `Bearer ${token}`,
 };
-
 
 export async function fetchChatsByUserId(userId) {
   try {
@@ -26,7 +27,6 @@ export async function fetchChatsByUserId(userId) {
   }
 }
 
-
 export async function fetchMessagesByChatId(chatId) {
   try {
     const response = await fetch(`${BASE_URL}menssage/${chatId}`, {
@@ -44,7 +44,6 @@ export async function fetchMessagesByChatId(chatId) {
     throw error;
   }
 }
-
 
 export async function createChat(chatData) {
   try {
@@ -64,7 +63,6 @@ export async function createChat(chatData) {
     throw error;
   }
 }
-
 
 export async function sendMessage(messageData) {
   try {
--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-const BASE_URL = `${import.meta.env.VITE_BASE_URL}`
+const BASE_URL = import.meta.env.VITE_BASE_URL
 
 const socket = io(BASE_URL, {transports: ['websocket'],});
 
