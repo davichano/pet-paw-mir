60c275604853b68dccf0c6379c7cf56c037a77d2
William Lupaca <wlupacat@gmail.com>
2024-11-27 12:04:19 -0500

    fix: image on landing page

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -29,9 +29,9 @@ const LandingPage = () => {
             </svg>
             <p>Pet Paws</p>
           </div>
-          <div className="max-w-44 max-h-44 aspect-square h-auto bto bg-custom-200 items-center justify-center md:hidden">
+          <div className="max-w-60 max-h-60 aspect-square h-auto bto bg-white items-center justify-center md:hidden">
             <div className="text-custom-50 font-bold text-2xl text-center">
-            <img src="/src/assets/img/foto_principal.png" className="w-screen p-1 my-20"></img>
+            <img src="/src/assets/img/foto_principal.png" className="w-screen"></img>
             </div>
           </div>
           <h1 className="font-semibold text-4xl text-center text-custom-200">
